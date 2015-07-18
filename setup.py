6bf77b41b86e4e5fbcc6d37d19d84ff7dd8d97ae
Geoffrey Chan <gc@qsensei.com>
2015-07-18 19:50:12 -0400

    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='enchanted-pelican',
-      version='0.1',
+      version='0.2',
       description='Spellchecking plugin for Pelican static site generator.',
       classifiers=[],
       keywords="pelican spellcheck",
